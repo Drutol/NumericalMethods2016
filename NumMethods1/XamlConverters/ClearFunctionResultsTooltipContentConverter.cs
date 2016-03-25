474192bb73b467c94bb0c77d8cc7c698c0d2f8d9
Piotr Szymczak <dogier140@poczta.fm>
2016-03-25 17:35:46 +0100

    Localization p2

---
+++ NumMethods1/XamlConverters/ClearFunctionResultsTooltipContentConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using NumMethods1.ViewModels;
+
+namespace NumMethods1.XamlConverters
+{
+    class ClearFunctionResultsTooltipContentConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {           
+            return $"{ViewModelLocator.Main.Locale["#ResetResultsFor"]} {value}";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
