1b0c7a865a20ba52a0bdf2dad0a4769682ceaef8
Adik_T <adam.trzeszczak@hotmail.com>
2016-03-31 20:26:40 +0200

    Last call update

--- NumMethods1/Exceptions/MaxIterationsReachedException.cs
+++ NumMethods1/Exceptions/MaxIterationsReachedException.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NumMethods1.Exceptions
 {
-    class MaxIterationsReachedException : Exception
+    internal class MaxIterationsReachedException : Exception
     {
     }
 }
