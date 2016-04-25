756f055c18dcba2e0af064ae1b876b83f4c3ba5e
Sztecondus <mrxadikt@gmail.com>
2016-04-25 16:17:55 +0200

    Update. (#20)
    rysuje wykres dla ztablicowanych, ale nie wiem jak nody zrobić żeby odpowiadały temu wykresowi
    wygląda jeszcze gorzej teraz xd
    precyzja jest na 3 stopni tak na prawdę
    jest wybór czy combo box czy data grid
    bardzo źle kod wygląda?

--- NumMethods3/ViewModel/MainViewModel.cs
+++ NumMethods3/ViewModel/MainViewModel.cs
@@ -6,6 +6,7 @@ using NumMethods3.MathCore;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Windows;
 using System.Windows.Input;
 using OxyPlot;
 
@@ -56,7 +57,7 @@ namespace NumMethods3.ViewModel
                 int nodeCount = int.Parse(InterpolationNodesCount);
                 double toX = double.Parse(ToXValueBind), fromX = double.Parse(FromXValueBind);
                 double nodeDist = Math.Abs((fromX - toX)/(nodeCount - 1));
-                int precision = 1000;
+                int precision = (int)Math.Pow(10,(_precValue/2)+1);
                 var nodes = new List<FunctionValue>();
                 var interpolated = new List<FunctionValue>();
                 var interpolationResults = new List<FunctionValue>();
@@ -71,18 +72,36 @@ namespace NumMethods3.ViewModel
 
                 var progressives = NumCore.ProgressiveSubs(nodeCount, nodes.Select(value => value.Y).ToList());
 
-                for (double i = 0; i < precision; i++)
+                if (!_isDataEntryEnabled)
                 {
-                    var current = new FunctionValue();
-                    current.X = fromX + i*(toX - fromX)/precision;
-                    double t = (current.X - nodes[0].X)/nodeDist;
-                    current.Y = NumCore.NewtonsInterpolation(t, progressives);
-                    interpolationResults.Add(current);
-                    interpolated.Add(new FunctionValue
+                    for (double i = 0; i < precision; i++)
                     {
-                        X = current.X,
-                        Y = FunctionSelectorSelectedItem.GetValue(current.X)
-                    });
+                        var current = new FunctionValue();
+                        current.X = fromX + i*(toX - fromX)/precision;
+                        double t = (current.X - nodes[0].X)/nodeDist;
+                        current.Y = NumCore.NewtonsInterpolation(t, progressives);
+                        interpolationResults.Add(current);
+                        interpolated.Add(new FunctionValue
+                        {
+                            X = current.X,
+                            Y = FunctionSelectorSelectedItem.GetValue(current.X)
+                        });
+                    }
+                }
+                else
+                {
+                    var vals = new List<FunctionValue>(_functionValues.ToList());
+                    foreach (var x in vals)
+                    {
+                        interpolated.Add(new FunctionValue
+                        {
+                            X = x.X,
+                            Y = x.Y
+                        });
+                        double t = (x.X - nodes[0].X) / nodeDist;
+                        x.Y = NumCore.NewtonsInterpolation(t, progressives);
+                        interpolationResults.Add(x);
+                    }
                 }
                 NodeChartData = nodes.Select(value => value.ToDataPoint).ToList();
                 ChartDataInterpolated = interpolated.Select(value => value.ToDataPoint).ToList();
@@ -129,6 +148,38 @@ namespace NumMethods3.ViewModel
             }
         }
 
+        private int _precValue = 3;
+
+        public int PrecValueBind
+        {
+            get { return _precValue; }
+            set
+            {
+                _precValue = value;
+                RaisePropertyChanged(() => PrecValueBind);
+            }
+        }
+
+        private bool _isDataEntryEnabled= true;
+
+        public bool IsDataGridEnabled
+        {
+            get { return _isDataEntryEnabled; }
+            set
+            {
+                _isDataEntryEnabled = value;
+                RaisePropertyChanged(() => IsDataGridEnabled);
+                RaisePropertyChanged(() => IsComboBoxEnabled);
+            }
+        }
+
+        public bool IsComboBoxEnabled
+        {
+            get { return !_isDataEntryEnabled; }
+        }
+
+
+
         public List<DataPoint> ChartDataInterpolation { get; set; } =
             new List<DataPoint>();
 
@@ -137,7 +188,6 @@ namespace NumMethods3.ViewModel
 
         public List<DataPoint> NodeChartData { get; set; } =
             new List<DataPoint>();
-
 
         public MainViewModel()
         {
