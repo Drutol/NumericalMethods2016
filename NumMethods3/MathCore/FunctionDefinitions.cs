ce1e1d27eb69e02b7c6623210ff1099aedb1bfc2
Sztecondus <mrxadikt@gmail.com>
2016-04-29 12:19:40 +0200

    Let it be the last
    * Let it be the last
    
    * functions

--- NumMethods3/MathCore/FunctionDefinitions.cs
+++ NumMethods3/MathCore/FunctionDefinitions.cs
@@ -36,10 +36,10 @@ namespace NumMethods3.NumCore
 
         public double GetValue(double x)
         {
-            return Math.Pow(4, x+1) - Math.Pow(5, x/2) - 10;
+            return x*x*x*Math.Sin(3*x -15) + x*x*x*4 + x*(x+4)*4 + Math.Abs(x*x*3+2) - 100;
         }
 
-        public string TextRepresentation => "4^(x+1)-5^(x/2)-10";
+        public string TextRepresentation => "x^3*sin(3x - 15) + 4x^3 + 4x^2 + 16x + |3x^2 + 2| - 100";
     }
 
     internal class Function4 : IFunction, ISelectable
