369b966af056c21a12962baaa5460a4198205d9c
Sztecondus <adam.trzeszczak@hotmail.com>
2016-05-11 13:52:51 +0200

    GUI changes excludind intervals something? I hate integrals :|

--- NumMethods4/MathCore/NumCore.cs
+++ NumMethods4/MathCore/NumCore.cs
@@ -8,101 +8,101 @@ namespace NumMethods4.MathCore
 {
     public class NumCore
     {
-        public static double SimpsonsMethod(double a, double b, IFunction selecetedFunction, double e)
+        public static double SimpsonsMethod(double fromX, double toX,
+            IFunction selecetedFunction, double acc)
         {
-            int ilePrzedzialow = 1;
-            double h;// = Math.abs(b-a)/2;
-            double[] punkty = new double[1000000];
-            double[] wartosciPunktow = new double[1000000];
-            double suma1 = 0, suma2 = 0;
-
+            int intervals = 1;
+            double[] nodes = new double[1000000];
+            double[] nodesVals = new double[1000000];
+            double sum1=0, sum2;
             do
             {
-                int ilePunktow = 2 * ilePrzedzialow + 1;
-                h = Math.Abs(b - a) / (2 * ilePrzedzialow);     //odleglosc miedzy punktami dla danej ilosci przedzialow
-                WypelnijTablicePunktow(punkty, a, b, h, ilePunktow);
-                suma2 = suma1;
-                suma1 = 0;
-                for (int i = 0; i < (ilePunktow); i++)
+                var nodeCount = 2 * intervals + 1;
+                var xDiff = Math.Abs(toX - fromX) / (2 * intervals);
+                WypelnijTablicePunktow(nodes, fromX, xDiff, nodeCount);
+                sum2 = sum1;
+                sum1 = 0;
+                for (int i = 0; i < (nodeCount); i++)
                 {
-                    wartosciPunktow[i] = selecetedFunction.GetValue(punkty[i]);
-
-                    if (i == 0 || i == ilePunktow - 1)
-                    {
-                        suma1 += wartosciPunktow[i];
-                    }
+                    nodesVals[i] = selecetedFunction.GetValue(nodes[i]);
+                    if (i == 0 || i == nodeCount - 1)
+                        sum1 += nodesVals[i];
                     else
                     {
                         if (i % 2 == 1)
-                        {
-                            suma1 += 4 * wartosciPunktow[i];
-                        }
+                            sum1 += 4 * nodesVals[i];
                         else
-                        {
-                            suma1 += 2 * wartosciPunktow[i];
-                        }
+                            sum1 += 2 * nodesVals[i];
                     }
                 }
-
-                suma1 *= (h / 3);       //wynik
-
-                ilePrzedzialow *= 2;
-                //System.out.println(suma1 + " " + suma2 + " " + h);
-            } while (Math.Abs(suma1 - suma2) > e);
-            //System.out.println("Calka funkcji " + nazwyFunkcji(wybor) + " na przedziale od " + a + " do " + b + " wynosi: " + suma1);
-            return suma1;
+                sum1 *= (xDiff / 3);
+                intervals *= 2;
+            } while (Math.Abs(sum1 - sum2) > acc);
+            return sum1;
         }
 
-        public static void WypelnijTablicePunktow(double[] punkty, double przedzialOd, double przedzialDo, double odstep, int ilePunktow)
+        private static void WypelnijTablicePunktow(double[] nodes, double fromX,
+            double xDiff, int nodeCount)
         {
-            for (int i = 0; i < (ilePunktow); i++)
+            for (int i = 0; i < nodeCount; i++)
             {
                 if (i == 0)
                 {
-                    punkty[0] = przedzialOd;
+                    nodes[0] = fromX;
                 }
                 else
                 {
-                    punkty[i] = punkty[i - 1] + odstep;
+                    nodes[i] = nodes[i - 1] + xDiff;
                 }
             }
         }
 
-        //public static void SimpsonsMethodPt2(int wybor, double e)
+        public static double ExcludingEndpointsIntegration(double fromX, double toX,
+            bool isLeftEx, bool isRightEx, IFunction selectedFunction, double includedEndpointsValue)
+        {
+            if (isLeftEx)
+                includedEndpointsValue -= selectedFunction.GetValue(fromX);
+            if (isRightEx)
+                includedEndpointsValue -= selectedFunction.GetValue(toX);
+
+            return includedEndpointsValue;
+        }
+
+        //public static void SimpsonsMethodPt2(int wybor, double acc)
         //{
-        //    double a = 0;
-        //    double b = -1;
+        //    double fromX = 0;
+        //    double toX = -1;
         //    double c = -1;
         //    int iterator = 1;
-        //    double h;// = Math.abs(b-a)/2;
+        //    double h;// = Math.abs(toX-fromX)/2;
         //    double sumad1 = 0, sumad2 = 0;
         //    double sumau1 = 0, sumau2 = 0;
         //    do
         //    {
         //        if (iterator != 1)
         //        {
-        //            a = b;
-        //            b = b + c;
+        //            fromX = toX;
+        //            toX = toX + c;
         //        }
         //        sumad1 = 0;
         //        sumau1 = 0;
-        //        h = (a + b) / 2;        //odleglosc miedzy punktami dla danej ilosci przedzialow
-        //        sumad1 = (Math.abs(b - a) / 3) * (waga(Math.abs(a)) * funkcja(wybor, Math.abs(a)) + 4 * waga(Math.abs(h)) * funkcja(wybor, Math.abs(h)) + waga(Math.abs(b)) * funkcja(wybor, Math.abs(b)));
-        //        sumau1 = (Math.abs(b - a) / 3) * (waga(a) * funkcja(wybor, a) + 4 * waga(h) * funkcja(wybor, h) + waga(b) * funkcja(wybor, b));
-        //        if (sumad1 > e)
+        //        h = (fromX + toX) / 2;        //odleglosc miedzy punktami dla danej ilosci przedzialow
+        //        sumad1 = (Math.abs(toX - fromX) / 3) * (waga(Math.abs(fromX)) * funkcja(wybor, Math.abs(fromX)) + 4 * waga(Math.abs(h)) * funkcja(wybor, Math.abs(h)) + waga(Math.abs(toX)) * funkcja(wybor, Math.abs(toX)));
+        //        sumau1 = (Math.abs(toX - fromX) / 3) * (waga(fromX) * funkcja(wybor, fromX) + 4 * waga(h) * funkcja(wybor, h) + waga(toX) * funkcja(wybor, toX));
+        //        if (sumad1 > acc)
         //        {
         //            sumad2 += sumad1;
         //        }
-        //        if (sumau1 > e)
+        //        if (sumau1 > acc)
         //        {
         //            sumau2 += sumau1;
         //        }
 
         //        iterator += 1;
-        //        //System.out.println(iterator-1 + " " + sumau1 + " " + sumau2 + " " + sumad1 + " " + sumad2 + " " + h + " " + a + " " + b);
-        //    } while (Math.abs(sumad1) > e && Math.abs(sumau1) > e);
+        //        //System.out.println(iterator-1 + " " + sumau1 + " " + sumau2 + " " + sumad1 + " " + sumad2 + " " + h + " " + fromX + " " + toX);
+        //    } while (Math.abs(sumad1) > acc && Math.abs(sumau1) > acc);
         //    System.out.println("Ta sama calka obliczona metoda Simpsona: ");
-        //    if (sumad1 < e)
+        //    if (sumad1 < acc)
         //    {
         //        System.out.println(sumau2);
         //    }
